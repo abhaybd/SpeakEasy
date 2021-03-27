7a85a92ee2938de3df46a774ecb31a8ef6b29995
Abhay D <a.deshpande012@gmail.com>
2021-03-27 13:07:12 -0700

    Added basic actions

--- speakeasy-backend/src/main/java/org/hoohacks/Main.java
+++ speakeasy-backend/src/main/java/org/hoohacks/Main.java
@@ -4,16 +4,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URI;
 import java.util.Objects;
 import javax.imageio.ImageIO;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            if (!SystemTray.isSupported()) {
-                System.err.println("System tray not supported!");
+            if (!SystemTray.isSupported() || !Desktop.isDesktopSupported()) {
+                System.err.println("System tray and/or Desktop not supported!");
                 return;
+            } else {
+                Desktop d = Desktop.getDesktop();
+                if (!d.isSupported(Desktop.Action.BROWSE)) {
+                    System.err.println("Browsing not supported!");
+                }
             }
+
             PopupMenu menu = new PopupMenu();
             BufferedImage iconImg = ImageIO.read(
                     Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("tray-icon.png")));
@@ -41,15 +48,22 @@ public class Main {
         }
     }
 
-    private static void start(ActionEvent e) {
-        System.out.println(e.getActionCommand());
+    private static void start(ActionEvent event) {
+        Server.start();
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.browse(URI.create("http://localhost:5000"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void stop(ActionEvent e) {
-        System.out.println(e.getActionCommand());
+        Server.stop();
     }
 
     private static void exit(ActionEvent e) {
-        System.out.println(e.getActionCommand());
+        Server.stop();
+        System.exit(0);
     }
 }
