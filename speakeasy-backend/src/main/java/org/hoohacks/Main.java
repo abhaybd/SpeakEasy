f121245cf12d9f4e2966ca75354265237d3115da
arnavthareja <arnav.thareja25@gmail.com>
2021-03-27 12:50:54 -0700

    Move server to different class

--- speakeasy-backend/src/main/java/org/hoohacks/Main.java
+++ speakeasy-backend/src/main/java/org/hoohacks/Main.java
@@ -1,15 +1,6 @@
 package org.hoohacks;
 
-import com.google.gson.Gson;
-import static spark.Spark.*;
-
 public class Main {
     public static void main(String[] args) {
-        get("/voices", (req, res) -> {
-            // get english voices
-            Gson gson = new Gson();
-            var voices = TTS.getInstance().getVoices();
-            return gson.toJson(voices);
-        });
     }
 }
