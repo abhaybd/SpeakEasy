15324b1f357323e85ccbecfd9c957514bd24d436
Karthikeya Vemuri <karkeys1@uw.edu>
2021-03-27 14:25:43 -0700

    textbox

--- speakeasy-frontend/src/App.js
+++ speakeasy-frontend/src/App.js
@@ -1,37 +1,150 @@
-import logo from './logo.svg';
+import React from 'react';
+import speakeasy from './speakeasy.png';
 import './App.css';
-import React, {Component} from 'react';
+import {Component} from 'react';
 
-class App extends Component<{}, {}> {
 
+class App extends Component {
     constructor() {
         super();
+        this.state = {
+            name: "",
+            message: "",
+            showMenu: false,
+            voices: null
+        };
+        this.getVoice();
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
     }
+
+    showMenu(event) {
+        event.preventDefault();
+
+        this.setState({showMenu: true}, () => {
+            document.addEventListener('click', this.closeMenu);
+        });
+    }
+
+    getVoice = async () => {
+        try {
+            const response = fetch("/voices");
+            let data = await response;
+
+            if (!data.ok) {
+                alert("There was an issue loading the app");
+                return;
+            }
+
+            let voicesPromise = data.json();
+            let voicesData = await voicesPromise;
+            this.setState({
+                voices: voicesData
+            })
+
+        } catch {
+            alert("unable to contact server")
+        }
+    }
+
+    speak = async () => {
+        let state = {
+            Name: this.state.name,
+            message: this.state.message
+        }
+        try {
+            const response = await fetch("/speak", JSON.stringify(state));
+            const data = await response.json()
+            this.setState({
+                message: ""
+            })
+
+        } catch {
+            alert("unable to contact server")
+
+        }
+    }
+
+    closeMenu() {
+        this.setState({showMenu: false}, () => {
+            document.removeEventListener('click', this.closeMenu);
+        });
+    }
+
+    handleNameChange = (givenName) => {
+        this.setState({
+            name: givenName
+        })
+    }
+    handleMessageChange = (given) => {
+        this.setState({
+            message: given
+        })
+    }
+
+    onClickSubmit = () => {
+        this.speak();
+    }
+
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={
-                        logo
-                    }
-                         className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <textarea
-                        rows={15}
-                        cols={50}
-                        />
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+
+                <img src={speakeasy} className="App-logo" alt="logo"/>
+                <h1>SpeakEasy
+                </h1>
+                <label className="name">
+                    Name:
+                    <br/>
+                    <input type="text" value={this.state.Name} onChange={(event) => {
+                        this.handleNameChange(event.target.value)
+                    }}/>
+                </label>
+                <br/>
+                Message:
+                <textarea
+                    row={15}
+                    col={50}
+                    onChange={(event) => {
+                        this.handleMessageChange(event.target.value)
+                    }}
+                    value={this.state.message}
+                />
+                <br/>
+                <button
+                    onClick={() => {
+                        this.onClickSubmit()
+                    }}
+                >
+                    Submit
+                </button>
+
+                <button onClick={this.showMenu} className="showbtn">
+                    Show menu
+                </button>
+
+                {
+                    this.state.showMenu
+                        ? (
+                            <div className="menu">
+                                <button> Menu item 1</button>
+                                <button> Menu item 2</button>
+                                <button> Menu item 3</button>
+                                <button>Menu item 4</button>
+                                <button>Menu item 5</button>
+                                <button>Menu item 6</button>
+                                <button>Menu item 7</button>
+                                <button>Menu item 8</button>
+                                <button>Menu item 9</button>
+                                <button>Menu item 10</button>
+                            </div>
+                        )
+                        : (
+                            null
+                        )
+                }
+
             </div>
         );
     }
