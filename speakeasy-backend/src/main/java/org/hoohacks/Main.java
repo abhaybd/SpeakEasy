f0b26827620953f3b7d29f7dce0e5bf5763723dc
Abhay D <a.deshpande012@gmail.com>
2021-03-27 13:18:12 -0700

    More work on main

--- speakeasy-backend/src/main/java/org/hoohacks/Main.java
+++ speakeasy-backend/src/main/java/org/hoohacks/Main.java
@@ -9,48 +9,54 @@ import java.util.Objects;
 import javax.imageio.ImageIO;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            if (!SystemTray.isSupported() || !Desktop.isDesktopSupported()) {
-                System.err.println("System tray and/or Desktop not supported!");
-                return;
-            } else {
-                Desktop d = Desktop.getDesktop();
-                if (!d.isSupported(Desktop.Action.BROWSE)) {
-                    System.err.println("Browsing not supported!");
-                }
-            }
-
-            PopupMenu menu = new PopupMenu();
-            BufferedImage iconImg = ImageIO.read(
-                    Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("tray-icon.png")));
-            TrayIcon icon = new TrayIcon(iconImg, "SpeakEasy");
-            icon.setImageAutoSize(true);
-            SystemTray tray = SystemTray.getSystemTray();
-
-            MenuItem start = new MenuItem("Start");
-            MenuItem stop = new MenuItem("Stop");
-            MenuItem exit = new MenuItem("Exit");
-
-            start.addActionListener(Main::start);
-            stop.addActionListener(Main::stop);
-            exit.addActionListener(Main::exit);
-
-            menu.add(start);
-            menu.add(stop);
-            menu.add(exit);
-
-            icon.setPopupMenu(menu);
-
-            tray.add(icon);
-        } catch (IOException | AWTException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException, AWTException {
+        new Main().startApp();
     }
 
-    private static void start(ActionEvent event) {
+    private final MenuItem start, stop, exit;
+    private final Desktop desktop;
+    private final TrayIcon icon;
+
+    private Main() throws IOException {
+        if (!SystemTray.isSupported() || !Desktop.isDesktopSupported()) {
+            throw new IllegalStateException("System tray and/or Desktop not supported!");
+        }
+
+        desktop = Desktop.getDesktop();
+        if (!desktop.isSupported(Desktop.Action.BROWSE)) {
+            throw new IllegalStateException("Browsing not supported!");
+        }
+
+        PopupMenu menu = new PopupMenu();
+        BufferedImage iconImg = ImageIO.read(
+                Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("tray-icon.png")));
+        icon = new TrayIcon(iconImg, "SpeakEasy");
+        icon.setImageAutoSize(true);
+
+        start = new MenuItem("Start");
+        stop = new MenuItem("Stop");
+        exit = new MenuItem("Exit");
+
+        start.addActionListener(this::start);
+        stop.addActionListener(this::stop);
+        exit.addActionListener(this::exit);
+
+        menu.add(start);
+        menu.add(stop);
+        menu.add(exit);
+
+        icon.setPopupMenu(menu);
+    }
+
+    public void startApp() throws AWTException {
+        SystemTray tray = SystemTray.getSystemTray();
+        tray.add(icon);
+        start(null);
+    }
+
+    private void start(ActionEvent event) {
         Server.start();
-        Desktop desktop = Desktop.getDesktop();
+        stop.setEnabled(true);
         try {
             desktop.browse(URI.create("http://localhost:5000"));
         } catch (IOException e) {
@@ -58,11 +64,12 @@ public class Main {
         }
     }
 
-    private static void stop(ActionEvent e) {
+    private void stop(ActionEvent e) {
         Server.stop();
+        stop.setEnabled(false);
     }
 
-    private static void exit(ActionEvent e) {
+    private void exit(ActionEvent e) {
         Server.stop();
         System.exit(0);
     }
