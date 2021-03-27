0bb10d76808d59ee7c6681b3eb824f24d472f33f
arnavthareja <arnav.thareja25@gmail.com>
2021-03-27 15:17:34 -0700

    delete everything in frontend

--- speakeasy-frontend/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm install
-npm run build
-
-rm -rf ../speakeasy-backend/src/main/resources/public
-mkdir -p ../speakeasy-backend/src/main/resources/public
-cp -r build/* ../speakeasy-backend/src/main/resources/public
