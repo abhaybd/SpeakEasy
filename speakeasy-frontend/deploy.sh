d5b68ae26afd9ca8f1dcfd3e2c5f486bcd89370a
Abhay D <a.deshpande012@gmail.com>
2021-03-27 14:35:41 -0700

    Deploy frontend before running backend

--- speakeasy-frontend/deploy.sh
+++ speakeasy-frontend/deploy.sh
@@ -2,5 +2,6 @@
 
 npm run build
 
+rm -rf ../speakeasy-backend/src/main/resources/public
 mkdir -p ../speakeasy-backend/src/main/resources/public
 cp -r build/* ../speakeasy-backend/src/main/resources/public
