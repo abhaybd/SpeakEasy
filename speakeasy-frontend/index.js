2840903af95cf0867bf19322a180af9fb774d4a8
arnavthareja <arnav.thareja25@gmail.com>
2021-03-27 19:21:03 -0700

    Refactor

--- speakeasy-frontend/index.js
+++ speakeasy-frontend/index.js
@@ -11,45 +11,42 @@ function send() {
     }
 }
 
-(function () {
-    getVoices().then(voices => {
-        if (voices !== null) {
-            let dropdown = document.getElementById("voiceSelector");
-            voices.sort((a, b) => {
-                if (a.gender !== b.gender) {
-                    return a.gender > b.gender ? 1 : -1;
-                } else {
-                    return a.name > b.name ? 1 : -1;
-                }
-            });
-            voices.forEach((voice, i) => {
-                let gender = voice.gender.toLowerCase();
-                gender = gender.charAt(0).toUpperCase() + gender.substring(1);
-                let child = document.createElement("option");
-                child.setAttribute("value", voice.name);
-                if (i === 0) {
-                    child.setAttribute("selected", "");
-                }
-                child.appendChild(document.createTextNode(`Voice ${i + 1} - ${gender}`));
-                dropdown.appendChild(child);
-            });
-
-            dropdown.onchange = function () {
-                playSample(this.value);
+getVoices().then(voices => {
+    if (voices !== null) {
+        let dropdown = document.getElementById("voiceSelector");
+        voices.sort((a, b) => {
+            if (a.gender !== b.gender) {
+                return a.gender > b.gender ? 1 : -1;
+            } else {
+                return a.name > b.name ? 1 : -1;
             }
-        } else {
-            alert("Error contacting server!");
-        }
-    });
+        });
+        voices.forEach((voice, i) => {
+            let gender = voice.gender.toLowerCase();
+            gender = gender.charAt(0).toUpperCase() + gender.substring(1);
+            let child = document.createElement("option");
+            child.setAttribute("value", voice.name);
+            if (i === 0) {
+                child.setAttribute("selected", "");
+            }
+            child.appendChild(document.createTextNode(`Voice ${i + 1} - ${gender}`));
+            dropdown.appendChild(child);
+        });
 
-    let submitButton = document.getElementById("submit");
-    submitButton.onclick = send;
-
-    // Send when enter is pressed
-    document.onkeydown = function (e) {
-        if (e.key === "Enter" && document.activeElement === document.getElementById("message")) {
-            e.preventDefault();
-            send();
+        dropdown.onchange = function () {
+            playSample(this.value);
         }
+    } else {
+        alert("Error contacting server!");
     }
-})();
+});
+
+document.getElementById("submit").onclick = send;
+
+// Send when enter is pressed
+document.onkeydown = function (e) {
+    if (e.key === "Enter" && document.activeElement === document.getElementById("message")) {
+        e.preventDefault();
+        send();
+    }
+}
